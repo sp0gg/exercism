84b46075b2017c2fb98b494eecc0cfb6a00fd931
Joe Nelson <joe.nelson@sherwin.com>
2016-12-22 14:53:27 -0500

    hamming 2

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,6 @@
 class Hamming
   def self.compute(first, second)
    raise ArgumentError unless first.length == second.length
-   weight = 0
-   first.chars.each_with_index {|e, i| weight+= 1 unless e == second[i]}
-   weight
+   first.chars.zip(second.chars).count {|set| set[0] != set[1]}
   end
 end
