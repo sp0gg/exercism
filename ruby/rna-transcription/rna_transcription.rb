74a475f8e1cd54634b8a4ec0c6478d86499c53ec
sp0gg <joe.nelson@runbox.com>
2016-12-24 08:20:17 -0500

    rna 2

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -3,10 +3,10 @@ module BookKeeping
 end
 
 class Complement
-  DNA_RNA_COMPLEMENT = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+  DNA = 'GCTA'
+  RNA = 'CGAU'
 
   def self.of_dna(dna)
-    return '' unless dna.delete(DNA_RNA_COMPLEMENT.keys.join).size == 0
-    dna.chars.map{|nucleotide| DNA_RNA_COMPLEMENT[nucleotide]}.join
+    dna =~ /[^#{DNA}]/ ? '' : dna.tr(DNA, RNA)
   end
 end
