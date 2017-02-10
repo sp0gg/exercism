0efa43e4cc9cf69b7998393a1704709ca2a7ef56
Joe Nelson <joe.nelson@sherwin.com>
2017-02-10 08:17:10 -0500

    csharp hello world 1

---
+++ Exercism/HelloWorld/HelloWorldTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Exercism.HelloWorld
+{
+    [TestFixture]
+    public class HelloWorldTest
+    {
+        [Test]
+        public void No_name()
+        {
+            Assert.That(global::HelloWorld.HelloWorld.Hello(null), Is.EqualTo("Hello, World!"));
+        }
+
+        [Test]
+        public void Sample_name()
+        {
+            Assert.That(global::HelloWorld.HelloWorld.Hello("Alice"), Is.EqualTo("Hello, Alice!"));
+        }
+
+        [Test]
+        public void Other_sample_name()
+        {
+            Assert.That(global::HelloWorld.HelloWorld.Hello("Bob"), Is.EqualTo("Hello, Bob!"));
+        }
+    }
+}
---
+++ csharp/hello-world/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld
+{
+    public static class HelloWorld
+    {
+        public static string Hello(string name = "World")
+        {
+            return $"Hello, {name ?? "World"}!";
+        }
+    }
+}
