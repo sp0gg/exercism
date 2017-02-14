d7095ffff6286beac1ce9bfcd0177b2250a04794
Joe Nelson <joe.nelson@sherwin.com>
2017-02-14 16:22:38 -0500

    bob 1 csharp

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,46 @@
+﻿using static System.Text.RegularExpressions.Regex;
+
+namespace Exercism.Bob
+{
+    public static class Bob
+    {
+        public static string Hey(string phrase)
+        {
+            var filteredPhrase = phrase.Trim();
+
+            return DetermineResponse(filteredPhrase);
+        }
+
+        private static string DetermineResponse(string phrase)
+        {
+            if (IsShouting(phrase))
+            {
+                return "Whoa, chill out!";
+            }
+            if (IsQuestion(phrase))
+            {
+                return "Sure.";
+            }
+            if (IsSilent(phrase))
+            {
+                return "Fine. Be that way!";
+            }
+            return "Whatever.";
+        }
+
+        private static bool IsShouting(string filteredPhrase)
+        {
+            return !IsMatch(filteredPhrase, @"[a-z]") && IsMatch(filteredPhrase, @"[A-Z]");
+        }
+
+        private static bool IsQuestion(string filteredPhrase)
+        {
+            return filteredPhrase.EndsWith("?");
+        }
+
+        private static bool IsSilent(string filteredPhrase)
+        {
+            return filteredPhrase.Equals(string.Empty);
+        }
+    }
+}
