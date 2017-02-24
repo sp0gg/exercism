13044f9a92f9f43018beafcf026351afbfe824bf
sp0gg <joe.nelson@runbox.com>
2017-02-24 16:15:45 -0500

    prime-factors 2

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,19 +1,24 @@
 require 'prime'
 class PrimeFactors
   def self.for(n)
-    factors = []
-    factor = 2
-    number = n
     return [] if n == 1
+
+    factors = []
+    factor_array = Prime.entries(894175)
+    i = 0
+    number = n
+
     until false
+      factor = factor_array[i]
       if number % factor == 0
         factors << factor
         number = number / factor
         break if number == 1
       else
-        factor += 1
+        i += 1
       end
     end
+
     factors
   end
 end
