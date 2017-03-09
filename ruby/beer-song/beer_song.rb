e3ac8d7088693ee5a1d21777926a32658bae5eba
Joe Nelson <joe.nelson@sherwin.com>
2017-03-09 09:44:24 -0500

    beer-song 1

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,16 +1,25 @@
 class BeerSong
 
-
-  def initialize
-
-  end
-
   def verse(n)
     first_line(n) + last_line(n)
   end
 
+  def verses(*n)
+    range = (n[1]..n[0]).to_a.reverse
+    range.map{|i| verse(i)}.join("\n")
+  end
+
+  def lyrics
+    verses(99,0)
+  end
+
   def first_line(n)
     "#{quantity(n)} #{bottles(n)} of beer on the wall, #{quantity(n).downcase} #{bottles(n)} of beer.\n"
+  end
+
+  def last_line(n)
+    return "Go to the store and buy some more, 99 bottles of beer on the wall.\n" if n == 0
+    "Take #{units(n)} down and pass it around, #{quantity(n-1).downcase} #{bottles(n-1)} of beer on the wall.\n"
   end
 
   def bottles(n)
@@ -25,8 +34,8 @@ class BeerSong
     n == 0 ? 'No more': n.to_s
   end
 
-  def last_line(n)
-    return "Go to the store and buy some more, 99 bottles of beer on the wall.\n" if n == 0
-    "Take #{units(n)} down and pass it around, #{quantity(n-1).downcase} #{bottles(n-1)} of beer on the wall.\n"
-  end
+end
+
+module BookKeeping
+  VERSION = 2
 end
--- ruby/beer-song/beer_song_test.rb
+++ ruby/beer-song/beer_song_test.rb
@@ -35,7 +35,6 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_a_couple_verses
-    skip
     expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" \
       "Take one down and pass it around, 98 bottles of beer on the wall.\n" \
       "\n" \
@@ -45,7 +44,6 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_a_few_verses
-    skip
     expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" \
       "Take one down and pass it around, 1 bottle of beer on the wall.\n" \
       "\n" \
@@ -58,7 +56,6 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_the_whole_song
-    skip
     expected = <<-SONG
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
@@ -370,7 +367,6 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
   #
   # Define a constant named VERSION inside of BookKeeping.
   def test_bookkeeping
-    skip
     assert_equal 2, BookKeeping::VERSION
   end
 end
