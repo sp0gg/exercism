7a4827903fff03e9483674d2cddb66dfbf6a3834
sp0gg <joe.nelson@runbox.com>
2017-03-18 19:46:19 -0400

    leap 2

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,7 +1,2 @@
 def is_leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            return year % 400 == 0
-        else:
-            return True
-    return False
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
