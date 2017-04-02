57a5a60a6795ee9de133a7600208f9a8c3cfb1af
sp0gg <joe.nelson@runbox.com>
2017-04-01 21:42:10 -0400

    rna 2

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -4,6 +4,5 @@ import re
 def to_rna(string):
     if len(re.findall('[^ACTG]', string)) > 0: return ''
 
-    replacements = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-    regexp = re.compile('|'.join(map(re.escape, replacements)))
-    return regexp.sub(lambda match: replacements[match.group(0)], string)
+    trans = string.maketrans('ACTG', 'UGAC')
+    return string.translate(trans)
